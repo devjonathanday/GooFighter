6fb24267fbe416af3fa1e3fc515e4967ba35761b
jday151r <j.day151r@gmail.com>
2019-10-02 17:36:27 -0700

    Added squash/stretch script, modified shadows of UI

---
+++ Assets/Scripts/StretchScale.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StretchScale : MonoBehaviour
+{
+    [Range(-1, 1)]
+    public float stretch;
+    public AnimationCurve XZStretch;
+    public AnimationCurve YStretch;
+    public Vector3 defaultScale;
+
+    void Update()
+    {
+        transform.localScale = Vector3.Scale(defaultScale, new Vector3(XZStretch.Evaluate(stretch), YStretch.Evaluate(stretch), XZStretch.Evaluate(stretch)));
+    }
+}
