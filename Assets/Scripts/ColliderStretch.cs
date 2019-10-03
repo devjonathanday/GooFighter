9acd282c9e14dd3b20f5527ec9565fa27265766e
jday151r <j.day151r@gmail.com>
2019-10-03 15:13:24 -0700

    Created player prefab variants

---
+++ Assets/Scripts/ColliderStretch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderStretch : MonoBehaviour
+{
+    public CapsuleCollider capsule;
+    public float defaultHeight;
+    public float scalar;
+    public Rigidbody connectedBody;
+    
+    void Update()
+    {
+        capsule.height = (connectedBody.position - transform.position).magnitude * defaultHeight * scalar;
+    }
+}
