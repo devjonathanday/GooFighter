e43fdb4b46f05b7a01f79b113ac297becf5e7830
Forrest McCarthy <mccarthy_forrest@outlook.com>
2019-10-24 17:29:18 -0700

    Correct pool balls

--- Assets/Scripts/PoolBall.cs
+++ Assets/Scripts/PoolBall.cs
@@ -1,20 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PoolBall : MonoBehaviour
 {
     public enum BALLTYPE { Solid, Stripes }
-    public enum BALLCOLOR { Red,Blue,Green, Yellow, Black, White}
+    public enum BALLCOLOR { Red,Blue,Green, Yellow, Orange, Purple, Brown, Black, White}
 
     public BALLTYPE BallType;
     public BALLCOLOR BallColor;
+    [Range(0,15)]
+    public int BallNumber;
 
     Renderer MyRend;
+    TextMeshPro NumberText;
 
     void Start()
     {
         MyRend = GetComponent<Renderer>();
+        NumberText = GetComponentInChildren<TextMeshPro>();
 
         switch (BallColor)
         {
@@ -36,10 +41,24 @@ public class PoolBall : MonoBehaviour
             case BALLCOLOR.White:
                 MyRend.materials[0].color = Color.white;
                 break;
+            case BALLCOLOR.Purple:
+                MyRend.materials[0].color = new Color(.3f,0,.6f);
+                break;
+            case BALLCOLOR.Orange:
+                MyRend.materials[0].color = new Color(1, .5f, 0);
+                break;
+            case BALLCOLOR.Brown:
+                MyRend.materials[0].color = new Color(.3f,0,0);
+                break;
             default:
                 break;
         }
         if (BallType == BALLTYPE.Solid) Destroy(MyRend.materials[2]);
         else Destroy(MyRend.materials[1]);
+
+        NumberText.text = BallNumber.ToString();
+        if (BallNumber == 0) NumberText.text = "";
+
+
     }
 }
