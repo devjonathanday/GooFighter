c2f72d566525a3202e3381256882c95956465012
jday151r <j.day151r@gmail.com>
2019-10-31 12:14:46 -0700

    Brightened gray color health bar

---
+++ Assets/Scripts/Menu/FallingGooMan.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingGooMan : MonoBehaviour
+{
+    public float respawnYPos;
+    public Vector3 startPos;
+    public Rigidbody rBody;
+    public float initalTorque;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        if(transform.position.y < respawnYPos)
+        {
+            transform.position = startPos;
+            rBody.velocity = Vector3.zero;
+            Vector3 randomRotation = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+            rBody.AddTorque(randomRotation * initalTorque);
+        }
+    }
+}
