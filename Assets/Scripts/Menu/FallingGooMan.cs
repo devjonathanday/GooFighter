25c68c98465e68b50e9d29778167b3dda18a8d43
jday151r <j.day151r@gmail.com>
2019-10-31 12:42:36 -0700

    Title screen falling goo men

--- Assets/Scripts/Menu/FallingGooMan.cs
+++ Assets/Scripts/Menu/FallingGooMan.cs
@@ -6,6 +6,7 @@ public class FallingGooMan : MonoBehaviour
 {
     public float respawnYPos;
     public Vector3 startPos;
+    public Vector2 xRange;
     public Rigidbody rBody;
     public float initalTorque;
 
@@ -18,7 +19,7 @@ public class FallingGooMan : MonoBehaviour
     {
         if(transform.position.y < respawnYPos)
         {
-            transform.position = startPos;
+            transform.position = new Vector3(Random.Range(xRange.x, xRange.y), startPos.y, startPos.z);
             rBody.velocity = Vector3.zero;
             Vector3 randomRotation = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
             rBody.AddTorque(randomRotation * initalTorque);
