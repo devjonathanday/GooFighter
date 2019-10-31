2cffd47ce02c14ca11ff80e86d4aca9861d56a8b
jday151r <j.day151r@gmail.com>
2019-10-31 14:47:24 -0700

    falling goo men converted to particles w custom shader

--- Assets/Scripts/Menu/FallingGooMan.cs
+++ Assets/Scripts/Menu/FallingGooMan.cs
@@ -7,22 +7,27 @@ public class FallingGooMan : MonoBehaviour
     public float respawnYPos;
     public Vector3 startPos;
     public Vector2 xRange;
+    public Vector2 zRange;
     public Rigidbody rBody;
     public float initalTorque;
 
     void Start()
     {
         startPos = transform.position;
+        Reset();
     }
 
     void Update()
     {
         if(transform.position.y < respawnYPos)
-        {
-            transform.position = new Vector3(Random.Range(xRange.x, xRange.y), startPos.y, startPos.z);
-            rBody.velocity = Vector3.zero;
-            Vector3 randomRotation = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-            rBody.AddTorque(randomRotation * initalTorque);
-        }
+            Reset();
+    }
+
+    private void Reset()
+    {
+        transform.position = new Vector3(Random.Range(xRange.x, xRange.y), startPos.y, Random.Range(zRange.x, zRange.y));
+        rBody.velocity = Vector3.zero;
+        Vector3 randomRotation = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        rBody.AddTorque(randomRotation * initalTorque);
     }
 }
