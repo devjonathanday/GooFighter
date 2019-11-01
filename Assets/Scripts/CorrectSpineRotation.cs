c80d75598b9cd98de03f6ad0166ff38e3ca8b484
diammen <marcelnamdao@gmail.com>
2019-10-31 17:57:16 -0700

    attempt to fix wobbly ragdoll

---
+++ Assets/Scripts/CorrectSpineRotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorrectSpineRotation : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+
+    private Quaternion targetRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        targetRot = Quaternion.Euler(90, 0, 0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRot, Time.deltaTime * speed));
+    }
+}
